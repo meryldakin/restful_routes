626b6ad40c1f09299dfbb886d64ee1fb2e96e801
meryldakin <meryl.k.dakin@gmail.com>
2018-01-15 19:03:52 -0500

    full CRUD functionality

--- app/controllers/sushis_controller.rb
+++ app/controllers/sushis_controller.rb
@@ -1,22 +1,48 @@
 class SushisController < ApplicationController
-
+    
     def index
         @sushis = Sushi.all
+    end
+    
+    def show
+        current_sushi
     end
 
     def new
         @sushi = Sushi.new
+    end
 
+    def create
+        @sushi = Sushi.create(sushi_params)
+        redirect_to sushi_path(@sushi)
     end
 
     def edit
-        @sushi = Sushi.find_by(params[:id])
+        current_sushi
+    end
+
+    def update
+        current_sushi
+        @sushi.update(sushi_params)
+        redirect_to sushi_path(@sushi)
+    end
+
+    def destroy
+        current_sushi
+        @sushi.destroy
+        redirect_to sushis_path
     end
 
     def home 
     end
 
-    def show
-        @sushi = Sushi.find_by(params[:id])
+    private
+
+    def sushi_params
+        params.require(:sushi).permit(:name)
+    end
+
+    def current_sushi
+        @sushi = Sushi.find_by(id: params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-# resource :sushis
-get '/sushis/new', to: "sushis#new"
-get '/sushis', to: "sushis#index"
-get '/sushis/:id/edit', to: "sushis#edit"
-get '/sushis/:id', to: "sushis#show", as: "sushi"
+resources :sushis
 get '/', to: "sushis#home"
 get '/:anything', to: "application#wrong_page"
 
