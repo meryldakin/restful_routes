86a968542c1a4b9290ad9b404b03bd9805292382
meryldakin <meryl.k.dakin@gmail.com>
2018-01-15 20:20:40 -0500

    trying out beautifiers

--- app/controllers/sushis_controller.rb
+++ app/controllers/sushis_controller.rb
@@ -1,48 +1,46 @@
 class SushisController < ApplicationController
-    
-    def index
-        @sushis = Sushi.all
-    end
-    
-    def show
-        current_sushi
-    end
+  def index
+    @sushis = Sushi.all
+  end
 
-    def new
-        @sushi = Sushi.new
-    end
+  def show
+    current_sushi
+  end
 
-    def create
-        @sushi = Sushi.create(sushi_params)
-        redirect_to sushi_path(@sushi)
-    end
+  def new
+    @sushi = Sushi.new
+  end
 
-    def edit
-        current_sushi
-    end
+  def create
+    @sushi = Sushi.create(sushi_params)
+    redirect_to sushi_path(@sushi)
+  end
 
-    def update
-        current_sushi
-        @sushi.update(sushi_params)
-        redirect_to sushi_path(@sushi)
-    end
+  def edit
+    current_sushi
+  end
 
-    def destroy
-        current_sushi
-        @sushi.destroy
-        redirect_to sushis_path
-    end
+  def update
+    current_sushi
+    @sushi.update(sushi_params)
+    redirect_to sushi_path(@sushi)
+  end
 
-    def home 
-    end
+  def destroy
+    current_sushi
+    @sushi.destroy
+    redirect_to sushis_path
+  end
 
-    private
+  def home; end
 
-    def sushi_params
-        params.require(:sushi).permit(:name)
-    end
+  private
 
-    def current_sushi
-        @sushi = Sushi.find_by(id: params[:id])
-    end
+  def sushi_params
+    params.require(:sushi).permit(:name)
+  end
+
+  def current_sushi
+    @sushi = Sushi.find_by(id: params[:id])
+  end
 end
