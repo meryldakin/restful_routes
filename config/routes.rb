e5b9ddcd211f65ee0eb18b204b86e90d21027579
meryldakin <meryl.k.dakin@gmail.com>
2018-01-15 19:34:31 -0500

    removed controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :sushis
-get '/', to: "sushis#home"
-get '/:anything', to: "application#wrong_page"
+  
+  get '/:anything', to: "application#wrong_page"
+  get '/', to: "sushis#home"
 
 end
